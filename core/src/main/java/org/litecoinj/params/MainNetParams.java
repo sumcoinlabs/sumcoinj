bitcoinj/bitcoinj

fixed litecoin bip32 headers

core/src/main/java/org/litecoinj/params/MainNetParams.java
@@ -43,8 +43,8 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 9333;
         packetMagic = 0xfbc0b6dbl;
-        bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
-        bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
+        bip32HeaderPub = 0x019da462; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderPriv = 0x019d9cfe; //The 4 byte header that serializes in base58 to "xprv"
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
         majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;

sumcoinlabs/sumcoinj

core/src/main/java/org/litecoinj/params/MainNetParams.java
@@ -43,8 +43,8 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         segwitAddressHrp = "ltc";
         port = 9333;
         packetMagic = 0xfbc0b6dbl;
-        bip32HeaderP2PKHpub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
-        bip32HeaderP2PKHpriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
+        bip32HeaderP2PKHpub = 0x019da462; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderP2PKHpriv = 0x019d9cfe; //The 4 byte header that serializes in base58 to "xprv"
         bip32HeaderP2WPKHpub = 0x04b24746; // The 4 byte header that serializes in base58 to "zpub".
         bip32HeaderP2WPKHpriv = 0x04b2430c; // The 4 byte header that serializes in base58 to "zprv"
 